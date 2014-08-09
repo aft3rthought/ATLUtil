99515d252782505cbc9fb2e448666b200e0419f1
Max Robinson <mcrpublic@gmail.com>
2014-08-08 19:05:53 -0700

    array_util: add c_array_last and c_array_last_const_ref

--- array_util.h
+++ array_util.h
@@ -7,12 +7,30 @@
 namespace atl
 {
     /*
-     arrayLength: Returns the length of a C-style array
+     c_array_len: Returns the length of a C-style array
      */
     template <typename T, size_t N>
     constexpr size_t c_array_len(T(&)[N])
     {
         return N;
+    }
+
+    /*
+     c_array_last: Returns by value the last element of a C-style array
+     */
+    template <typename T, size_t N>
+    constexpr T c_array_last(T(&in_array)[N])
+    {
+        return in_array[N-1];
+    }
+    
+    /*
+     c_array_last_const_ref: Returns a const ref to the last element of a C-style array
+     */
+    template <typename T, size_t N>
+    constexpr const T & c_array_last_const_ref(T(&in_array)[N])
+    {
+        return in_array[N-1];
     }
     
     /*
