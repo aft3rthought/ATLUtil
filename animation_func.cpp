81a4bed20dfe582423181d856d5f106d19b00cc1
Max Robinson <mcrpublic@gmail.com>
2014-11-27 22:22:49 -0800

    Shake function

--- animation_func.cpp
+++ animation_func.cpp
@@ -1,6 +1,7 @@
 
 
 #include "animation_func.h"
+#include "math2d.h"
 #include <cmath>
 
 float atl::ease_in_style1(const float in_val, const float in_bounce)
@@ -12,4 +13,12 @@ float atl::ease_in_style2(const float in_val)
 {
     float cos = cosf(in_val * M_PI / 2.f);
     return 1.f - (cos * cos * cos * cos);
+}
+
+atl::point2f atl::shake(const float in_magnitude, const float in_phase)
+{
+    float sineVal = sinf(in_phase);
+    float xAmount = in_magnitude * sineVal;
+    float yAmount = in_magnitude * sineVal;
+    return atl::point2f(sineVal * xAmount, cosf(in_phase) * yAmount);
 }
--- animation_func.h
+++ animation_func.h
@@ -2,6 +2,9 @@
 
 namespace atl
 {
+    class point2f;
+    
     float ease_in_style1(const float in_val, const float in_bounce);
     float ease_in_style2(const float in_val);
+    atl::point2f shake(const float in_magnitude, const float in_phase);
 }
