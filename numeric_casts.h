81b721b6f03b66f457407ba216ef0959f11b91ff
Max Robinson <mcrpublic@gmail.com>
2015-02-24 21:14:10 -0800

    Add some more casts to numeric_casts

--- numeric_casts.h
+++ numeric_casts.h
@@ -6,4 +6,6 @@ namespace atl
 {
     constexpr uint32_t narrow_unsigned(uint64_t val) { return (uint32_t)val; }
     constexpr int32_t narrow_signed(int64_t val) { return (int32_t)val; }
+    constexpr int32_t make_signed(uint32_t val) { return (int32_t)val; }
+    constexpr int32_t narrow_size_t_to_int(size_t val) { return (int32_t)val; }
 }
