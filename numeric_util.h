68863e6927b7b07d12d8b22db0da457101ccc6b9
Max Robinson <mcrpublic@gmail.com>
2017-05-13 21:21:57 -0700

    Revision to numeric_util.h

--- numeric_util.h
+++ numeric_util.h
@@ -2,62 +2,15 @@
 
 #pragma once
 
-#include "ATLUtil/debug_break.h"
 #include <limits>
-
-#define atl_numeric_cast_assert atl_fatal_assert
 
 namespace atl
 {
-    template <typename result_type, typename input_type>
-    result_type ignore_sign(input_type input_value)
+    template <typename result_type, typename input_type, typename on_failure_callable_type>
+    result_type integer_cast(input_type input_value, on_failure_callable_type on_failure_callable)
     {
-        static_assert(sizeof(result_type) == sizeof(input_type), "Types must have the same size");
-        return (result_type)input_value;
-    }
-    
-    template <typename result_type, typename input_type>
-    result_type integer(input_type);
-    
-    template <>
-    inline unsigned int integer(int input_value)
-    {
-        atl_numeric_cast_assert(input_value >= 0, "Signed to unsigned conversion: Input value is negative");
-        return (unsigned int)input_value;
-    }
-
-    template <>
-    inline int integer(unsigned int input_value)
-    {
-        atl_numeric_cast_assert(input_value <= std::numeric_limits<int>::max(), "Unsigned to signed conversion: Input value is too large");
-        return (int)input_value;
-    }
-
-    template <>
-    inline int integer(unsigned long input_value)
-    {
-        atl_numeric_cast_assert(input_value <= std::numeric_limits<int>::max(), "Unsigned to signed conversion: Input value is too large");
-        return (int)input_value;
-    }
-    
-    template <>
-    inline unsigned long integer(int input_value)
-    {
-        atl_numeric_cast_assert(input_value >= 0, "Signed to unsigned conversion: Input value is negative");
-        return (unsigned long)input_value;
-    }
-    
-    template <>
-    inline unsigned long integer(long input_value)
-    {
-        atl_numeric_cast_assert(input_value >= 0, "Signed to unsigned conversion: Input value is negative");
-        return (unsigned long)input_value;
-    }
-    
-    template <>
-    inline long integer(unsigned long input_value)
-    {
-        atl_numeric_cast_assert(input_value <= std::numeric_limits<long>::max(), "Unsigned to signed conversion: Input value is too large");
-        return (long)input_value;
+        const auto result = (result_type)input_value;
+        if((input_type)result != input_value) on_failure_callable();
+        return result;
     }
 }
