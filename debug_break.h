d3ee14c12155636429ea4ef5aa2c4a9cca1c29e9
Max Robinson <mcrpublic@gmail.com>
2017-08-12 18:35:19 -0700

    improve syntax of atl_log/fatal

--- debug_break.h
+++ debug_break.h
@@ -3,17 +3,21 @@
 #include <cstdio>
 #include <stdexcept>
 
+#ifndef atl_log_function_impl
+#define atl_log_function_impl printf
+#endif
+
 #ifndef atl_log_impl
-#define atl_log_impl printf
+#define atl_log_impl(Message) { atl_log_function_impl("%s, line %d: %s\n", __FILE__, __LINE__, Message); }
 #endif
 
 #ifndef atl_fatal_impl
-#define atl_fatal_impl(File, IntegerLine, Message) { atl_log_impl("%s, line %d: %s\n", File, IntegerLine, Message); exit(0); }
+#define atl_fatal_impl(Message) { atl_log_function_impl("%s, line %d: %s\n", __FILE__, __LINE__, Message); exit(0); }
 #endif
 
 #endif
 
-#define atl_debug_log(Message) { atl_log_impl("%s, line %d: %s\n", __FILE__, __LINE__, Message) }
-#define atl_fatal(Message) { atl_fatal_impl(__FILE__, __LINE__, Message) }
+#define atl_debug_log(Message) { atl_log_impl(Message) }
+#define atl_fatal(Message) { atl_fatal_impl(Message) }
 #define atl_fatal_if(Condition, Message) { if(Condition) atl_fatal(Message) }
 #define atl_fatal_assert(Condition, Message) { if(!(Condition)) atl_fatal(Message) }
