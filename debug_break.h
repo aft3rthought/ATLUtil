049a0dfa79f05fd433009c453eacc25384dc3b62
Max Robinson <mcrpublic@gmail.com>
2017-09-25 17:58:29 -0700

    callstack printing

--- debug_break.h
+++ debug_break.h
@@ -21,3 +21,26 @@
 #define atl_fatal(Message) { atl_fatal_impl(Message) }
 #define atl_fatal_if(Condition, Message) { if(Condition) atl_fatal(Message) }
 #define atl_fatal_assert(Condition, Message) { if(!(Condition)) atl_fatal(Message) }
+
+#pragma once
+
+#if defined(PLATFORM_OSX) || defined(PLATFORM_IOS)
+#include <execinfo.h>
+#endif
+
+namespace atl
+{
+    inline void print_callstack()
+    {
+#if defined(PLATFORM_OSX) || defined(PLATFORM_IOS)
+        void * callstack[256];
+        int frames = backtrace(callstack, 256);
+        char ** strs = backtrace_symbols(callstack, frames);
+        for(int i = 0; i < frames; i++, strs++)
+        {
+            printf("%s\n",*strs);
+        }
+        free(strs);
+#endif
+    }
+}
