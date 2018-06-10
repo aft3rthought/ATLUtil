a08200029527b1ba0c95be4bcb1c36e1f802a81a
Max Robinson <mcrpublic@gmail.com>
2018-06-10 01:50:54 -0700

    region - renamed to region_type and made pod

--- region.h
+++ region.h
@@ -4,18 +4,10 @@
 namespace atl
 {
 	template <typename region_value_type>
-	struct region
+	struct region_type
 	{
 		region_value_type * begin_pointer;
 		region_value_type * end_pointer;
-
-		region() {}
-
-		region(region_value_type * in_begin_pointer, region_value_type * in_end_pointer)
-			:
-			begin_pointer(in_begin_pointer),
-			end_pointer(in_end_pointer)
-		{}
 
 		region_value_type * begin() const { return begin_pointer; }
 		region_value_type * end() const { return end_pointer; }
@@ -23,5 +15,34 @@ namespace atl
 		region_value_type * back() const { return end_pointer - 1; }
 		size_t size() const { return end_pointer - begin_pointer; }
 		bool empty() const { return end_pointer <= begin_pointer; }
+
+		operator region_type<const region_value_type> () const
+		{
+			return region_type<const region_value_type>{begin_pointer, end_pointer};
+		}
 	};
+
+	template <typename region_value_type>
+	region_type<region_value_type> region(region_value_type * begin_pointer, region_value_type * end_pointer)
+	{
+		return region_type<region_value_type>{begin_pointer, end_pointer};
+	}
+
+	template <typename region_value_type, typename integer_type>
+	region_type<region_value_type> region_n(region_value_type * begin_pointer, integer_type count)
+	{
+		return region_type<region_value_type>{begin_pointer, begin_pointer + count};
+	}
+
+	template <typename region_value_type, size_t c_array_extent>
+	region_type<region_value_type> region_from_array(region_value_type (&c_array)[c_array_extent])
+	{
+		return region_type<region_value_type>{c_array, c_array + c_array_extent};
+	}
+
+	template <typename region_value_type, size_t c_array_extent>
+	region_type<region_value_type> region_from_string_literal_minus_terminator(region_value_type(&c_array)[c_array_extent])
+	{
+		return region_from_array(c_array).remove_tail(1);
+	}
 }
