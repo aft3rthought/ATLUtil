c46363778801425eaab5491a4fe51e1627f42c6e
Max Robinson <mcrpublic@gmail.com>
2020-05-31 23:45:33 -0700

    add ease_in_syle 2 integral function, and replace sinf/cosf with std:: versions

--- animation_func.cpp
+++ animation_func.cpp
@@ -6,19 +6,27 @@
 
 float atl::ease_in_style1(const float in_val, const float in_bounce)
 {
-    return in_val * in_val + sinf(in_val * in_val * atl::numbers::pi_f) * in_bounce;
+    return in_val * in_val + std::sinf(in_val * in_val * atl::numbers::pi_f) * in_bounce;
 }
 
 float atl::ease_in_style2(const float in_val)
 {
-    float cos = cosf(in_val * atl::numbers::half_pi_f);
+	auto cos = std::cosf(in_val * atl::numbers::half_pi_f);
     return 1.f - (cos * cos * cos * cos);
+}
+
+float atl::ease_in_style2_integral(const float in_val)
+{
+	auto sin = std::sinf(atl::numbers::pi_f * in_val);
+	auto cos = std::cosf(atl::numbers::pi_f * in_val);
+	return (5.f * in_val) / 8.f - sin / atl::numbers::two_pi_f - (sin * cos) / atl::numbers::eight_pi_f;
 }
 
 atl::point2f atl::shake(const float in_magnitude, const float in_phase)
 {
-    float sineVal = sinf(in_phase);
-    float xAmount = in_magnitude * sineVal;
-    float yAmount = in_magnitude * sineVal;
-    return atl::point2f(sineVal * xAmount, cosf(in_phase) * yAmount);
+	auto sine_val = std::sinf(in_phase);
+	auto cos_val = std::cosf(in_phase);
+	auto x_amount = in_magnitude * sine_val;
+	auto y_amount = in_magnitude * sine_val;
+    return atl::point2f(sine_val * x_amount, cos_val * y_amount);
 }
--- animation_func.h
+++ animation_func.h
@@ -9,6 +9,7 @@ namespace atl
     
     float ease_in_style1(const float in_val, const float in_bounce);
     float ease_in_style2(const float in_val);
+	float ease_in_style2_integral(const float in_val);
     atl::point2f shake(const float in_magnitude, const float in_phase);
     
     template <typename KeyframeType>
