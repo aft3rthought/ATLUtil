602e2c99c725aa70f2d9cc580415b0a10ba3ed03
Max Robinson <mcrpublic@gmail.com>
2021-09-22 19:55:56 -0700

    Comparators for region

--- region.h
+++ region.h
@@ -26,25 +26,37 @@ namespace atl
 	};
 
 	template <typename region_value_type>
-	region_type<region_value_type> region(region_value_type * begin_pointer, region_value_type * end_pointer)
+	bool operator == (const region_type<region_value_type>& a, const region_type<region_value_type>& b)
+	{
+		return a.begin_pointer == b.begin_pointer && a.end_pointer == b.end_pointer;
+	}
+
+	template <typename region_value_type>
+	bool operator != (const region_type<region_value_type>& a, const region_type<region_value_type>& b)
+	{
+		return !(a == b);
+	}
+
+	template <typename region_value_type>
+	constexpr region_type<region_value_type> region(region_value_type * begin_pointer, region_value_type * end_pointer)
 	{
 		return region_type<region_value_type>{begin_pointer, end_pointer};
 	}
 
 	template <typename region_value_type, typename integer_type>
-	region_type<region_value_type> region_n(region_value_type * begin_pointer, integer_type count)
+	constexpr region_type<region_value_type> region_n(region_value_type * begin_pointer, integer_type count)
 	{
 		return region_type<region_value_type>{begin_pointer, begin_pointer + count};
 	}
 
 	template <typename region_value_type, size_t c_array_extent>
-	region_type<region_value_type> region_from_array(region_value_type (&c_array)[c_array_extent])
+	constexpr region_type<region_value_type> region_from_array(region_value_type (&c_array)[c_array_extent])
 	{
 		return region_type<region_value_type>{c_array, c_array + c_array_extent};
 	}
 
 	template <typename region_value_type, size_t c_array_extent>
-	region_type<region_value_type> region_from_string_literal_minus_terminator(region_value_type(&c_array)[c_array_extent])
+	constexpr region_type<region_value_type> region_from_string_literal_minus_terminator(region_value_type(&c_array)[c_array_extent])
 	{
 		return region_from_array(c_array).remove_tail(1);
 	}
